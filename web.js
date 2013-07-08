bcb4e0b39c91fe6545d580d31000bcd8322d2d9f
Abhishek <abhinit06@gmail.com>
2013-07-08 05:39:23 +0000

    added fileread write

--- web.js
+++ web.js
@@ -3,7 +3,9 @@ var express = require('express');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World!');
+  fbuffer = fs.readFile("index.html");
+  resp = fbuffer.toString();
+  response.send(resp);
 });
 
 var port = process.env.PORT || 5000;
